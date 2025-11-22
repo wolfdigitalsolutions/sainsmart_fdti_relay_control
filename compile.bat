379bd028553c96ff2b563774fd2d5df01bc8dd26
matthew wolf <mwolf1963@gmail.com>
2025-11-22 17:03:50 -0500

    added bat file for pip installs and pyinstaller to create an exe

---
+++ compile.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+rem auth mwolf
+rem date 20251122
+rem purpose install dependencies and create .exe file
+
+rem dependencies
+pip install ftd2xx
+pip install pyinstaller
+rem compile to exe
+pyinstaller sain_smart_ftdi_relay_control.py --onefile
+echo .EXE file created successfully in the dist folder.
+pause
+@echo on
