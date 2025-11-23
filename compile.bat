c254def641fef9b4f44c511c0f1d7356d6b96de0
wolfdigitalsolutions <wolfdigitalsolutions@gmail.com>
2025-11-23 14:54:45 -0500

    added example usage .bat

--- compile.bat
+++ compile.bat
@@ -8,6 +8,7 @@ pip install ftd2xx
 pip install pyinstaller
 rem compile to exe
 pyinstaller sainsmart_ftdi_relay_control.py --onefile
+copy "ftd2xx.dll" "dist/ftd2xx.dll"
 echo .EXE file created successfully in the dist folder.
 pause
 @echo on
---
+++ dist/usage_example.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+sainsmart_ftdi_relay_control.exe --state 1 3
+timeout /T 3
+sainsmart_ftdi_relay_control.exe --state 2 --momentary 4 --duration .5
+timeout /T 3
+sainsmart_ftdi_relay_control.exe --on 3
+timeout /T 3
+sainsmart_ftdi_relay_control.exe --toggle 4 2
+timeout /T 3
+sainsmart_ftdi_relay_control.exe --off 1 2 3 4
+@echo  on
